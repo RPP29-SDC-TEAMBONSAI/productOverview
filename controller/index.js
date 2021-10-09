d10814e9253bd1a1bee251caebdc94fb1113c18f
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-10-09 10:37:58 -0400

    Update new production

--- controller/index.js
+++ controller/index.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  database: process.env.DB_TEST,
-  server: process.env.DB_HOST2,
+  database: process.env.DB_NAME,
+  server: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   pool: {
     max: 30,
