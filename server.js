f5988012c7004f0dea255ac79cbe1517d1eb14db
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-10-09 11:37:23 -0400

    Add path package

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const products = require('./routes/products');
 const sdc = require('./routes/sdc');
